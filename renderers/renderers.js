7dc80b0a3b9ab9b94081fc0fd439b89b1670f0f2
Stephen Trewick <stephent818@gmail.com>
2020-09-19 19:11:07 -0700

    platforms array, no bouncey

--- renderers/renderers.js
+++ renderers/renderers.js
@@ -44,4 +44,25 @@ const Floor = ({ body, size }) => {
   );
 };
 
-export { Frog, Floor };
+const Platform = ({ body, size }) => {
+  const width = size[0];
+  const height = size[1];
+
+  const x = body.position.x - width / 2;
+  const y = body.position.y - height / 2;
+
+  return (
+    <View
+      style={{
+        position: "absolute",
+        left: x,
+        top: y,
+        width,
+        height,
+        backgroundColor: "blue",
+      }}
+    />
+  );
+};
+
+export { Frog, Floor, Platform };
--- screens/Game.js
+++ screens/Game.js
@@ -1,13 +1,16 @@
 import { AppState, Dimensions, Text, TextPropTypes } from "react-native";
-import { Floor, Frog } from "../renderers/renderers.js";
+import { Floor, Frog, Platform } from "../renderers/renderers.js";
 import React, { PureComponent } from "react";
-import { fromPairs, get } from "lodash";
 
 import { GameEngine } from "react-native-game-engine";
 import Matter from "matter-js";
 import { Physics } from "../systems/Systems.js";
+import { get } from "lodash";
+import randomInt from "random-int";
 
 const { width, height } = Dimensions.get("window");
+
+let COUNTER = 1;
 
 class Game extends PureComponent {
   static navigationOptions = {
@@ -34,6 +37,16 @@ class Game extends PureComponent {
       label: "frog",
       friction: 0.1,
     });
+    const platform_0 = Matter.Bodies.rectangle(
+      width / 2,
+      height - 100,
+      25,
+      10,
+      {
+        isStatic: true,
+        label: "platform",
+      }
+    );
     const { platforms, bodies } = this.platforms;
     const floor = Matter.Bodies.rectangle(width / 2, height - 10, width, 100, {
       isStatic: true,
@@ -41,7 +54,7 @@ class Game extends PureComponent {
       // isSensor: true,
       label: "floor",
     });
-    Matter.World.add(world, [frog, floor]);
+    Matter.World.add(world, [frog, floor, ...bodies, platform_0]);
 
     return {
       physics: {
@@ -52,6 +65,11 @@ class Game extends PureComponent {
         body: frog,
         size: [25, 25],
         renderer: Frog,
+      },
+      platform_0: {
+        body: platform_0,
+        size: [25, 10],
+        renderer: Platform,
       },
       ...platforms,
       floor: {
@@ -67,12 +85,38 @@ class Game extends PureComponent {
       const { pairs } = event;
       const objA = pairs[0].bodyA.label;
       const objB = pairs[1].bodyB.label;
+      // TODO: setup up frog bounce on platforms
     });
   };
-
+  // create platforms array
   get platforms() {
     const platforms = {};
     const bodies = [];
+    // start with 8 platforms
+    for (let i = 0; i < 8; i++) {
+      const { platform, body } = this.platform;
+      Object.assign(platforms, { [`platform_${COUNTER}`]: platform });
+      bodies.push(body);
+
+      COUNTER += 1;
+    }
+
+    return { platforms, bodies };
+  }
+  get platform() {
+    const body = Matter.Bodies.rectangle(
+      randomInt(13, width - 13),
+      randomInt(0, height - 10),
+      25,
+      10,
+      {
+        isStatic: true,
+        label: "platform",
+      }
+    );
+    const platform = { body, size: [25, 10], renderer: Platform };
+
+    return { platform: platform, body };
   }
 
   render() {
